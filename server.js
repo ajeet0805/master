34ad596b418da932d7ff84a21e0cec214c288e4b
Ajeet Lodhi <ajeet10lodhi@gmail.com>
2025-09-25 04:15:48 +0530

    api data

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+//import fetch from "node-fetch";
+//const fetch = require("node-fetch");
+const app = express();
+const PORT = 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Default route
+app.get("/", (req, res) => {
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Response:", data);
+    res.send(data);
+  })
+  .catch((err) => {
+    console.error("Error:", err.message);
+  });
+  
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
