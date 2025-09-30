99c55446f49a845f200c6de3607f41ab52e900db
Ajeet Lodhi <ajeet10lodhi@gmail.com>
2025-10-01 00:16:27 +0530

    allow cors

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 //import fetch from "node-fetch";
 //const fetch = require("node-fetch");
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
-
+app.use(cors());// Allow all origin
+// app.use(cors({
+//   origin: [
+//     "http://localhost:3000",       // for local dev
+//     "https://front-end-ea7z.vercel.app/" // your Vercel frontend
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
 // Default route
 app.get("/", (req, res) => {
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
   .then((data) => {
-    console.log("Response:", data);
+    //console.log("Response:", data);
     res.send(data);
   })
   .catch((err) => {
